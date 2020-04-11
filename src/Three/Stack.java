8e5659ae0f3f900ffa746da187a1d31756aeb8a6
85001 <850015841@qq.com>
2020-04-11 15:35:09 +0800

    提交。

---
+++ src/Three/Stack.java
@@ -0,0 +1,44 @@
+package Three;
+
+/**
+ * 利用数组实现堆栈
+ */
+public class Stack {
+    private int[]elements;
+    private int elementSize;
+    private final static int DEFAULT_CAPACITY=10;
+    public Stack(int capacity)
+    {
+        elements=new int[capacity];
+    }
+    public Stack()
+    {
+        this(DEFAULT_CAPACITY);
+    }
+    public void push(int value)//进栈
+    {
+        if(elementSize>elements.length)
+        {
+            int []temp=new int[elements.length*2];
+            System.arraycopy(elements, 0, temp, 0, elements.length);
+            elements=temp;
+        }
+        elements[elementSize++]=value;
+    }
+    public int pop()//出栈，出栈前必须调用empty（）判断栈是否为空
+    {
+        return elements[--elementSize];
+    }
+    public boolean empty()//判断栈是否为空
+    {
+        return elementSize==0;
+    }
+    public int getSize()//获取栈中元素个数
+    {
+        return elementSize;
+    }
+    public int peek()//获取当前栈顶元素
+    {
+        return elements[elementSize-1];
+    }
+}
---
+++ src/Three/StackOfTest.java
@@ -0,0 +1,23 @@
+package Three;
+
+/**
+ * 测试类
+ */
+public class StackOfTest {
+    public static void main(String []args){
+        Stack stack=new Stack(15);
+        int []A=new int [15];
+        for(int i=0;i<A.length;i++)
+        {
+            A[i]=i;
+        }
+        for(int j=0;j<A.length;j++)
+        {
+            stack.push(A[j]);
+        }
+        while(!stack.empty())
+        {
+            System.out.print(stack.pop()+" ");
+        }
+    }
+}
